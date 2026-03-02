9a5f66b66a45b4bfc8c0e3c68e9abadc423e29fe
Tek1ss <deniskalinin516@gmail.com>
2026-03-02 20:13:32 +0300

    initial commit: car catalog project

---
+++ sql/insert_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO cars (brand, model, year, color, price) VALUES
+('Toyota', 'Camry', 2020, 'Silver', 1500000.00),
+('BMW', 'X5', 2022, 'Black', 4500000.50),
+('Lada', 'Vesta', 2021, 'White', 800000.00),
+('Ford', 'Focus', 2019, 'Blue', 950000.00),
+('Hyundai', 'Solaris', 2021, 'Gray', 1100000.00),
+('Kia', 'Rio', 2022, 'Red', 1250000.00),
+('Mercedes', 'E-Class', 2023, 'Black', 5200000.00),
+('Audi', 'Q7', 2024, 'White', 5800000.00),
+('Volkswagen', 'Polo', 2020, 'Silver', 1050000.00),
+('Nissan', 'Qashqai', 2022, 'Blue', 2100000.00);
